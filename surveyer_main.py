789aaa09250b8349ad7d8fe1f55921be99b7082b
Klein <sandeepsnathan08@gmail.com>
2025-08-04 14:36:46 +0530

    corrected syntax errors

--- surveyer_main.py
+++ surveyer_main.py
@@ -3,43 +3,56 @@ from time import sleep
 
 
 print("Connecting to the vehicle . . .")
-connection_string = "/dev/tty/ACM0"
-vehicle = connect(connection_string, wait_ready = True, baud = 56700)
+connection_string = "/dev/ttyACM0"
+vehicle = connect(connection_string, wait_ready = True, baud = 57600)
 
 def arm_and_takeoff(target_alt):
     print("Conducting saftey checks . . ." )
     while not vehicle.is_armable:
         print("Waiting for vehicle to become armable . . .Conducting safety checks . . .")
-        sleep(5)
+        sleep(3)
         
     vehicle.mode = VehicleMode("GUIDED")
     vehicle.armed = True
-    while not vehicle.armed and not vehicle.mode == GUIDED:
+    while not vehicle.armed or vehicle.mode.name != "GUIDED":
         print("Motors are being armed . . .")
-        sleep(5)
+        sleep(3)
     
     vehicle.simple_takeoff(target_alt)
     while True:
         print(f"Altitude: {vehicle.location.global_relative_frame.alt}")
-        if vehicle.location.global_relative_frame >= target_alt.0.95:
+        if vehicle.location.global_relative_frame.alt >= target_alt*0.95:
+            sleep(1)
             break
-            sleep(5)
+            
 
 
 #Parsing kml file function
-def parse_kml()
-#
+def parse_kml():
+    pass
 #
 #
 #
 
 
 #Yellow detection using opencv
-def detect_yellow()
+def detect_yellow():
+    pass
 #
 #
 #
 #
+
+#Main function
+def scan_kml_and_fly():
+    pass
+#
+#
+#
+#
+#
+#
+
 
 
             
